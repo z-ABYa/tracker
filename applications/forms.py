eda6d46415f17a36098443b6b4760e4521884824
z-ABYa <189abhish@gmail.com>
2026-02-21 12:08:53 +0530

    changed date format to dd/mm/yyyy in "add application"

--- applications/forms.py
+++ applications/forms.py
@@ -2,6 +2,11 @@ from django import forms
 from .models import Application
 
 class ApplicationForm(forms.ModelForm):
+    application_date = forms.DateField(
+        widget=forms.DateInput(
+            attrs={'type': 'date'}
+        )
+    )
     class Meta:
         model = Application
         fields = [
