2922dc2ffaf30136438d983ba8e1c7dc72f58c67
z-ABYa <189abhish@gmail.com>
2026-02-28 21:31:18 +0530

    Built pagination for dashboard

--- applications/views.py
+++ applications/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ApplicationForm
 from .models import Application
@@ -10,9 +11,13 @@ def dashboard(request):
     if status_filter := request.GET.get('status'):
         applications = applications.filter(status=status_filter)
     
+    paginator = Paginator(applications, 5)
+    page_no = request.GET.get('page')
+    page_obj = paginator.get_page(page_no)
+    
     statuses = Application.APPLICATION_STATUS
     return render(request, 'applications/dashboard.html', {
-            'applications': applications,
+            'page_obj': page_obj,
             'current_status': status_filter,
             'statuses': statuses,
         })
